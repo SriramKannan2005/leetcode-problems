17aeae7103e4d9c0af5f6ac7271b83cd186ba50f
SriramKannan2005 <166842369+SriramKannan2005@users.noreply.github.com>
2026-03-18 15:18:49 +0530

    Time: 7 ms (84.67%) | Memory: 47.3 MB (83.45%) - LeetSync

---
+++ 2508-maximum-sum-of-an-hourglass/maximum-sum-of-an-hourglass.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int maxSum(int[][] grid) {
+        int rows=grid.length;
+        int cols=grid[0].length;
+        int sum=0;
+        int maxSum=0;
+        if(rows<3||cols<3)
+        {
+            return 0;
+        }
+        for(int i=0;i<rows-2;i++)
+        {
+            for(int j=0;j<cols-2;j++)
+            {
+                sum=grid[i][j]+grid[i][j+1]+grid[i][j+2]+
+                grid[i+1][j+1]+
+                grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2];
+                // maxSum=Math.max(maxSum,sum);
+                // if(sum>maxSum)
+                // {
+                //     maxSum=sum;
+                // }
+                maxSum=sum>maxSum?sum:maxSum;
+            }
+        }
+
+        return maxSum;
+    }
+}
