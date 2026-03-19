c65eb357cba409a1d34532fb6272ba2cb576ea82
SriramKannan2005 <166842369+SriramKannan2005@users.noreply.github.com>
2026-03-19 16:50:50 +0530

    Time: 15 ms (64.87%) | Memory: 93.6 MB (45.99%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.java
+++ 217-contains-duplicate/contains-duplicate.java
@@ -5,14 +5,12 @@ class Solution {
         for (int i = 0; i < n; i++) {
             if (map.containsKey(nums[i])) {
                 map.put(nums[i], map.get(nums[i]) + 1);
-            } else {
-                map.put(nums[i], 1);
-            }
-        }
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            if (entry.getValue() >=2) {
                 return true;
-            }
+        }
+        else
+        {
+            map.put(nums[i],1);
+        }
         }
         return false;
     }
