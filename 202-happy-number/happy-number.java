5989bf6b06c54cf7dc5ea3a4d82b2cfb4de7c9c0
SriramKannan2005 <166842369+SriramKannan2005@users.noreply.github.com>
2026-03-19 17:03:13 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (86.83%) - LeetSync

---
+++ 202-happy-number/happy-number.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while (n != 1 && n != 4) {
+            n = getNext(n);
+        }
+        return n == 1;
+    }
+
+    private int getNext(int n) {
+        int sum = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+            sum += digit * digit;
+            n /= 10;
+        }
+
+        return sum;
+    }
+}
