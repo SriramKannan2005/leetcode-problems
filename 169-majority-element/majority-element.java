dde60dc5a75909a53f221546a4823379e8a0e5bd
SriramKannan2005 <166842369+SriramKannan2005@users.noreply.github.com>
2026-03-19 16:34:06 +0530

    Time: 17 ms (15.79%) | Memory: 52.6 MB (89.73%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,23 +1,26 @@
+import java.util.*;
+
 class Solution {
     public int majorityElement(int[] nums) {
-        Map<Integer,Integer> map=new HashMap<>();
-        int n=nums.length;
-        for(int i=0;i<n;i++)
-        {
-            if(map.containsKey(nums[i]))
-            {
-                map.put(nums[i],map.get(nums[i])+1);;
-            }
-            else
-            {
-                map.put(nums[i],1);
-            }
-            if(map.get(nums[i])>n/2)
-            {
-                return nums[i];
+        Map<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+
+        // build frequency map
+        for (int i = 0; i < n; i++) {
+            if (map.containsKey(nums[i])) {
+                map.put(nums[i], map.get(nums[i]) + 1);
+            } else {
+                map.put(nums[i], 1);
             }
         }
-        return -1;
-        
+
+        // find majority using entrySet
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > n / 2) {
+                return entry.getKey();
+            }
+        }
+
+        return -1; // not needed
     }
 }
