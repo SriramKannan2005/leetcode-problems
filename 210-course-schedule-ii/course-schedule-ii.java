2091bbc079f8f7f1096e772164ada64664d2f144
SriramKannan2005 <166842369+SriramKannan2005@users.noreply.github.com>
2026-03-25 11:46:48 +0530

    Time: 10 ms (10.86%) | Memory: 48.2 MB (11.38%) - LeetSync

---
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        ArrayList<ArrayList<Integer>> adj= new ArrayList<>();
+        int count=0;
+        for(int i=0;i<numCourses;i++)
+        {
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edge:prerequisites)
+        {
+            adj.get(edge[1]).add(edge[0]);
+        }
+     int[] indegree = new int[numCourses];
+     for(int i=0;i<numCourses;i++)
+     {
+         for(int n:adj.get(i))
+         {
+             indegree[n]++;
+         }
+     }
+     Queue<Integer> q= new LinkedList<>();
+     for(int i=0;i<numCourses;i++)
+     {
+         if(indegree[i]==0)
+         {
+             q.add(i);
+         }
+     }
+      ArrayList<Integer>res=new ArrayList<>();
+     while(!q.isEmpty())
+     {
+         int vertex=q.poll();
+         count++;
+         res.add(vertex);
+         for(int neighbour:adj.get(vertex))
+         {
+             indegree[neighbour]--;
+             if(indegree[neighbour]==0)
+             {
+                 q.add(neighbour);
+             }
+         }
+         
+     }
+       
+     return count!= numCourses ? new int [0]: res.stream().mapToInt(i -> i).toArray();
+    }
+}
