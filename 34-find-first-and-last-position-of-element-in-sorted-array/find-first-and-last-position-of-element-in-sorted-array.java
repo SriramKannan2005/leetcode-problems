b987f301dbe6c9cb5f0bf254017847a70a2ed15a
SriramKannan2005 <166842369+SriramKannan2005@users.noreply.github.com>
2026-05-13 12:55:20 +0530

    Time: 5 ms (0.80%) | Memory: 48 MB (85.83%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -3,34 +3,18 @@ class Solution {
 
         ArrayList<Integer> list = new ArrayList<>();
 
-        // convert array to ArrayList
         for (int num : nums) {
             list.add(num);
         }
 
-        // target not present
-        if (!list.contains(target)) {
+        int first = list.indexOf(target);
+
+        // target not found
+        if (first == -1) {
             return new int[]{-1, -1};
         }
 
-        // first occurrence
-        int first = list.indexOf(target);
-
-        // remove first occurrence
-        list.remove(first);
-
-        int last;
-
-        // check again
-        if (list.contains(target)) {
-
-            // index after removal
-            last = list.lastIndexOf(target) + 1;
-
-        } else {
-
-            last = first;
-        }
+        int last = list.lastIndexOf(target);
 
         return new int[]{first, last};
     }
