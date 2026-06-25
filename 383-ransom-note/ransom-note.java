f6dc517d5f3d6e851affd38c70d62e945e4e25b8
Sriram Kannan <166842369+SriramKannan2005@users.noreply.github.com>
2026-06-25 15:19:26 +0530

    Time: 5 ms (45.19%) | Memory: 46.7 MB (42.64%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+
+        char[] mag = magazine.toCharArray();
+
+        for (int i = 0; i < ransomNote.length(); i++) {
+
+            boolean found = false;
+
+            for (int j = 0; j < mag.length; j++) {
+
+                if (ransomNote.charAt(i) == mag[j]) {
+                    found = true;
+                    mag[j] = '#';   // Mark this character as used
+                    break;
+                }
+            }
+
+            if (!found) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
