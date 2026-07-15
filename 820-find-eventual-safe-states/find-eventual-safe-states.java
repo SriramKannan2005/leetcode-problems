b45a5abf3b53ea8f0fb4f3251cf8281ee16f243d
Sriram Kannan <166842369+SriramKannan2005@users.noreply.github.com>
2026-07-15 14:03:24 +0530

    Time: 4 ms (99.79%) | Memory: 63.4 MB (35.99%) - LeetSync

---
+++ 820-find-eventual-safe-states/find-eventual-safe-states.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public List<Integer> eventualSafeNodes(int[][] graph) {
+
+        int n = graph.length;
+        int[] color = new int[n];
+        List<Integer> ans = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            if (dfs(graph, color, i)) {
+                ans.add(i);
+            }
+        }
+
+        return ans;
+    }
+
+    private boolean dfs(int[][] graph, int[] color, int node) {
+
+        if (color[node] != 0) {
+            return color[node] == 2;
+        }
+
+        color[node] = 1;
+
+        for (int next : graph[node]) {
+            if (!dfs(graph, color, next)) {
+                return false;
+            }
+        }
+
+        color[node] = 2;
+        return true;
+    }
+}
