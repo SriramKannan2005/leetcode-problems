cdf6f8d494f7079466d61e0d6fb7b6b34e893362
Sriram Kannan <166842369+SriramKannan2005@users.noreply.github.com>
2026-09-22 14:17:30 +0530

    Time: 0 ms (100.00%) | Memory: 45 MB (49.51%) - LeetSync

---
+++ 112-path-sum/path-sum.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+
+        if (root == null) {
+            return false;
+        }
+
+        // Check if current node is a leaf
+        if (root.left == null && root.right == null) {
+            return targetSum == root.val;
+        }
+
+        targetSum = targetSum - root.val;
+
+        return hasPathSum(root.left, targetSum) ||
+               hasPathSum(root.right, targetSum);
+    }
+}
