d0f3e07a3c840ae7adce1d8e7e6103468cc9db14
Sriram Kannan <166842369+SriramKannan2005@users.noreply.github.com>
2026-09-22 14:09:14 +0530

    Time: 56 ms (5.33%) | Memory: 42.9 MB (8.97%) - LeetSync

---
+++ 69-sqrtx/sqrtx.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int mySqrt(int x) {
+
+        if (x < 2) {
+            return x;
+        }
+
+        for (int i = 1; i <= x / 2; i++) {
+
+            if ((long) i * i > x) {
+                return i - 1;
+            }
+        }
+
+        return x / 2;
+    }
+}
